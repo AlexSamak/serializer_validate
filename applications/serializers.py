e80d5ed74179ccfbd724fb79c877e431c4b5191f
AlexSamak <2009kazan@gmail.com>
2020-10-13 15:15:09 +0400

    validate_cargo

--- applications/serializers.py
+++ applications/serializers.py
@@ -6,3 +6,10 @@ class ApplicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = ApplicationModel
         fields = '__all__'
+
+    def validate_cargo(self, value):
+        for item in value:
+            print(item.id)
+            if item.id != 1:
+                raise serializers.ValidationError("Company bad !!!")
+        return value
