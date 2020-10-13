0745477937a01dcf11aafaf5df986380cdfa573f
AlexSamak <2009kazan@gmail.com>
2020-10-13 17:08:31 +0400

    external def validate

--- applications/serializers.py
+++ applications/serializers.py
@@ -2,14 +2,26 @@ from rest_framework import serializers
 from .models import ApplicationModel
 
 
+def multiple_of_ten(value):
+    print(value)
+    if value != 1:
+        raise serializers.ValidationError("Number the bad !!!")
+    return value
+
+
 class ApplicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = ApplicationModel
         fields = '__all__'
 
-    def validate_cargo(self, value):
-        for item in value:
-            print(item.id)
-            if item.id != 1:
-                raise serializers.ValidationError("Company bad !!!")
-        return value
+        extra_kwargs = {
+            'number': {'validators': [multiple_of_ten]}
+        }
+
+
+    # def validate_cargo(self, value):
+    #     for item in value:
+    #         print(item.id)
+    #         if item.id != 1:
+    #             raise serializers.ValidationError("Company bad !!!")
+    #     return value
